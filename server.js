c2c9736514f182111f0674b0cea18c25a9dc17bc
Олгица Златковска <olgica@Olgicas-MacBook-Pro.local>
2023-11-22 20:26:54 -0500

    create a restaurants route

--- server.js
+++ server.js
@@ -1,32 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const knex = require('knex')(require('./knexfile'));
-
 require('dotenv').config();
 
+const restaurantRoutes = require('./routes/restaurants');
 const PORT = process.env.PORT || 8080;
 
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
 app.use(cors());
 
-app.get('/', (_request, response) => {
-    response.send('Welcome home!')
-})
-
-app.get('/andrea', (_req, res) => {
-    async function getSome() {
-        try {
-            const response = await knex('addresses');
-            res.json(response);
-        }
-        catch (error) {
-            res.send(`this is the error: ${error}`);
-        }
-    }
-    getSome();
-})
+app.use('/api/restaurants', restaurantRoutes);
 
 app.listen(PORT, () => {
     console.log(`running at http://localhost:${PORT}`);
