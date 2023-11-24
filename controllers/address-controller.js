184335a97c4be0a3791d85eec51a2173cdbf54fc
Олгица Златковска <olgica@Olgicas-MacBook-Pro.local>
2023-11-24 11:54:34 -0500

    update addresses controller

--- controllers/address-controller.js
+++ controllers/address-controller.js
@@ -2,7 +2,9 @@ const knex = require('knex')(require('../knexfile'));
 
 const getRestaurantAddresses = async (request, response) => {
     try {
-        const addressesData = await knex('addresses');
+        const addressesData = await knex('addresses')
+            .join('restaurants', 'restaurants.id', 'addresses.restaurant_id')
+            .select('addresses.id', 'street_address', 'city', 'state', 'postal_code', 'country', 'restaurant_name', 'image_url')
 
         response.status(200).json(addressesData);
     } catch (error) {
